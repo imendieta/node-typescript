250209801bc502ad80b9c3702834004fce2154dc
imendieta <50755932+imendieta@users.noreply.github.com>
2020-09-12 01:03:39 +0200

    add base project

---
+++ dist/server.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+    console.log('You are listening port number 3000');
+});
